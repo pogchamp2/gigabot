5d4dda7148d934b0de6f22e3fcae892c61fc3c58
UnixPNG <maniarhythm@gmail.com>
2023-01-11 21:49:21 -0800

    omg the commas

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const { REST, Routes } = require('discord.js');
 const commands = [
   {
     name: 'ping',
-    description: 'Replies with Pong!',
-  },
+    description: 'Replies with Pong!'
+  }
 ];
 
 const rest = new REST({ version: '10' }).setToken(token);
