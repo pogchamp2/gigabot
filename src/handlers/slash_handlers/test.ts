4cfc487c0f0ef0e663aff81910641eb3a82ab4b5
Stephen Johnson <sforrestj@gmail.com>
2023-01-12 19:54:00 -0800

    Fix?

--- src/handlers/slash_handlers/test.ts
+++ src/handlers/slash_handlers/test.ts
@@ -1,18 +1,27 @@
 import { get } from "../../http";
 
 const test = async (inter: any, cli: any) => {
-    if(inter.user.id != 331196145630052355 && inter.user.id != 254112318089068545){
-        await inter.reply({ content: "You cannot use the test command, as you are not a developer of this bot.", ephemeral: true})
+  if (
+    inter.user.id != 331196145630052355n &&
+    inter.user.id != 254112318089068545n
+  ) {
+    await inter.reply({
+      content:
+        "You cannot use the test command, as you are not a developer of this bot.",
+      ephemeral: true,
+    });
+  } else {
+    let testType: String = inter.options.getString("test");
+    if (testType == "id") {
+      await inter.reply({ content: inter.user.id.toString(), ephemeral: true });
     }
-    else{
-    let testType: String = inter.options.getString("test")
-    if(testType == "id"){
-        await inter.reply({ content: inter.user.id.toString(), ephemeral: true})
+    if (testType == "http") {
+      await inter.reply({
+        content: (await get("http://66.42.73.191:8080/")).toString(),
+        ephemeral: true,
+      });
     }
-    if(testType == "http"){
-        await inter.reply({ content: (await get("http://66.42.73.191:8080/")).data.toString(), ephemeral: true})
-    }
-    }
-}
+  }
+};
 
 export { test };
--- src/http.ts
+++ src/http.ts
@@ -1,4 +1,4 @@
-const axios = require('axios');
+import axios from "axios";
 
 /* This method fires an HTTP GET request to url
 using the axios package.
@@ -6,15 +6,16 @@ Return array: [response (or null), error (or null)]
 */
 
 const get = async (url: String) => {
-    let result: Array<any> = []
-    axios.get(url)
-      .then((response: Object) => {
-        result[0] = response.data.toString();
-      })
-      .catch((error: Object) => {
-        result[1] = error;
-      });
-    return result;
-}
+  // const result: Array<any> = []
+  const result = axios
+    .get(url as any)
+    .then((response: Object) => {
+      return response;
+    })
+    .catch((error: Object) => {
+      return error;
+    });
+  return result;
+};
 
 export { get };
